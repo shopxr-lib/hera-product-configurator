ea1c20b96121c3affeec2ad7170fda9915da84c0
Eesha Shahid <eesha.shahid14@gmail.com>
2025-04-15 17:02:58 +0500

    fix: fixed import for modal

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -5,11 +5,11 @@ import CustomizePopUp from "./CustomizePopup2";
 import ShoppingCartFloating from "./ShoppingCartFloating";
 import ShoppingCartDrawer from "./ShoppingCartDrawer";
 import CustomPagination from "./CustomPagination";
-import CustomModal from "./CustomModal";
 import Branding from "./Branding";
 import ServerSync from "./ServerSync";
 import ProtectedRoute from "./ProtectedRoute";
 import SearchInput from "./SearchInput";
+import CustomModal from "./CustomModal";
 
 export {
   Canvas,
